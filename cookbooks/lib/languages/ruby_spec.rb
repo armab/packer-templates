e0cab7c2503b7bd6c4a5cf4cde1e19ec849c5ba0
Ed Morley <501702+edmorley@users.noreply.github.com>
2017-11-16 01:24:33 +0000

    Fixup

--- cookbooks/lib/languages/ruby_spec.rb
+++ cookbooks/lib/languages/ruby_spec.rb
@@ -3,7 +3,7 @@
 describe 'ruby installation' do
   describe command('gem --version') do
     its(:stderr) { should be_empty }
-    its(:stdout) { should match(/^Rspec \d+\.\d+/) }
+    its(:stdout) { should match(/^\d+\.\d+\.\d+/) }
   end
 
   describe command('bundle --version') do
@@ -13,6 +13,6 @@ describe 'ruby installation' do
 
   describe command('rspec --version') do
     its(:stderr) { should be_empty }
-    its(:stdout) { should match(/^\d+\.\d+\.\d+/) }
+    its(:stdout) { should match(/^RSpec \d+\.\d+/) }
   end
 end
